6262e843ce38bf08049e4e256dc4bd2e1cad9d6b
grant-tm <grant.t.morgan@gmail.com>
2024-07-28 20:37:52 -0700

    create placeholder for SearchResultList

---
+++ Source/SearchResultList.cpp
@@ -0,0 +1,1 @@
+#include "SearchResultList.h"
---
+++ Source/SearchResultList.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_RESULT_LIST_H
+#define SEARCH_RESULT_LIST_H
+
+#include <JuceHeader.h>
+
+class SearchResultList : public juce::ListBox
+{
+
+};
+
+#endif SEARCH_RESULT_LIST_H
