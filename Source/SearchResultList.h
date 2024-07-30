409516c495841d986fe954724ed0ee68f2bd2a2c
grant-tm <grant.t.morgan@gmail.com>
2024-07-29 19:39:38 -0700

    add custom search result box model

--- Source/SearchResultList.h
+++ Source/SearchResultList.h
@@ -3,9 +3,39 @@
 
 #include <JuceHeader.h>
 
-class SearchResultList : public juce::ListBox
+class SearchResultListBoxModel : public juce::ListBoxModel
 {
+public:
+    SearchResultListBoxModel(const juce::StringArray &items) : listItems(items) {}
 
+    int getNumRows() override
+    {
+        return listItems.size();
+    }
+
+    void paintListBoxItem(int rowNumber, juce::Graphics &g, int width, int height, bool rowIsSelected) override
+    {
+        if (rowIsSelected)
+        {
+            g.fillAll(juce::Colours::lightblue);
+        }
+        g.setColour(juce::Colours::black);
+        g.drawText(listItems[rowNumber], 0, 0, width, height, juce::Justification::centredLeft);
+    }
+
+    void listBoxItemClicked(int row, const juce::MouseEvent &) override
+    {
+        DBG("Item clicked: " << listItems[row]);
+    }
+
+    void setItems(juce::StringArray stringArray)
+    {
+        listItems = stringArray;
+    }
+
+
+private:
+    juce::StringArray listItems;
 };
 
 #endif SEARCH_RESULT_LIST_H
