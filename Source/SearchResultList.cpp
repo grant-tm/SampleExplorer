7646a3a1e3d85e077ec496787c42166dca14d824
grant-tm <grant.t.morgan@gmail.com>
2024-07-29 23:42:11 -0700

    clean up SearchResultList class file

--- Source/SearchResultList.cpp
+++ Source/SearchResultList.cpp
@@ -1,1 +1,69 @@
 #include "SearchResultList.h"
+
+//=============================================================================
+// LIST BOX
+
+SearchResultsListBox::SearchResultsListBox(juce::String name) : juce::ListBox(name)
+{
+    setLookAndFeel(&lookAndFeel);
+    setColour(juce::ListBox::backgroundColourId, GUI_PRIMARY_ELEV0);
+    setColour(juce::ListBox::outlineColourId, GUI_TEXT_GRAY);
+    setColour(juce::ListBox::textColourId, GUI_TEXT_WHITE);
+}
+
+//=============================================================================
+// LOOK AND FEEL]
+
+void SearchResultsLookAndFeel::drawScrollbar(juce::Graphics &g, juce::ScrollBar &scrollbar,
+    int x, int y, int width, int height,
+    bool isScrollbarVertical, int thumbStartPosition, int thumbSize,
+    bool isMouseOver, bool isMouseDown)
+{
+    juce::ignoreUnused(scrollbar);
+    
+    g.fillAll(GUI_PRIMARY_ELEV0);
+
+    if (thumbSize <= 0)
+        return;
+
+    juce::Rectangle<int> thumbBounds;
+    if (isScrollbarVertical)
+        thumbBounds.setBounds(x, thumbStartPosition, width, thumbSize);
+    else
+        thumbBounds.setBounds(thumbStartPosition, y, thumbSize, height);
+
+    juce::Path roundedRect;
+    roundedRect.addRoundedRectangle(thumbBounds, 4.f);
+
+    juce::Colour thumbColour = (isMouseOver || isMouseDown) ? GUI_PRIMARY_ELEV4 : GUI_PRIMARY_ELEV2;
+    g.setColour (thumbColour);
+    g.fillPath (roundedRect);
+}
+
+//=============================================================================
+// LIST BOX MODEL
+
+int SearchResultsListBoxModel::getNumRows()
+{
+    return listItems.size();
+}
+
+void SearchResultsListBoxModel::paintListBoxItem(int rowNumber, juce::Graphics &g, int width, int height, bool rowIsSelected)
+{
+    if (rowIsSelected)
+    {
+        g.fillAll(GUI_ACCENT);
+    }
+    g.setColour(GUI_TEXT_WHITE);
+    g.drawText(listItems[rowNumber], 0, 0, width, height, juce::Justification::centredLeft);
+}
+
+void SearchResultsListBoxModel::listBoxItemClicked(int row, const juce::MouseEvent &)
+{
+    DBG("Item clicked: " << listItems[row]);
+}
+
+void SearchResultsListBoxModel::setItems(juce::StringArray stringArray)
+{
+    listItems = stringArray;
+}
--- Source/SearchResultList.h
+++ Source/SearchResultList.h
@@ -10,91 +10,39 @@
 class SearchResultsLookAndFeel : public juce::LookAndFeel_V4
 {
 public:
-    void drawScrollbar(juce::Graphics &g, juce::ScrollBar &scrollbar, 
+    void drawScrollbar(juce::Graphics &g, juce::ScrollBar &scrollbar,
         int x, int y, int width, int height,
         bool isScrollbarVertical, int thumbStartPosition, int thumbSize,
-        bool isMouseOver, bool isMouseDown) override
-    {
-        // set the color for the scroll bar background
-        g.fillAll(GUI_PRIMARY_ELEV0);
-
-        // set the color for the scroll bar thumb
-        juce::Colour thumbColour = isMouseOver ? GUI_ACCENT : GUI_PRIMARY_ELEV1;
-        if (thumbSize > 0)
-        {
-            juce::Rectangle<int> thumbBounds;
-
-            if (isScrollbarVertical)
-                thumbBounds.setBounds (x, thumbStartPosition, width, thumbSize);
-            else
-                thumbBounds.setBounds (thumbStartPosition, y, thumbSize, height);
-
-            g.setColour (thumbColour);
-            g.fillRect (thumbBounds);
-        }
-    }
+        bool isMouseOver, bool isMouseDown) override;
 };
+
+//=============================================================================
+// LIST BOX
 
 class SearchResultsListBox : public juce::ListBox
 {
 public:
     SearchResultsListBox() = delete;
-    SearchResultsListBox(juce::String name) : juce::ListBox(name)
-    {
-        setLookAndFeel(&lookAndFeel);
-        
-        // Set the background color
-        setColour(juce::ListBox::backgroundColourId, GUI_PRIMARY_ELEV0);
-
-        // Set the outline color
-        setColour(juce::ListBox::outlineColourId, GUI_TEXT_GRAY);
-
-        // Set the text color
-        setColour(juce::ListBox::textColourId, GUI_TEXT_WHITE);
-    }
-
+    SearchResultsListBox(juce::String name);
     ~SearchResultsListBox() override = default;
-
 private:
     SearchResultsLookAndFeel lookAndFeel;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SearchResultsListBox)
 };
 
+//=============================================================================
+// LIST BOX MODEL
+
 class SearchResultsListBoxModel : public juce::ListBoxModel
 {
 public:
-    SearchResultsListBoxModel(const juce::StringArray &items) : listItems(items) 
-    {
-    }
-
-    int getNumRows() override
-    {
-        return listItems.size();
-    }
-
-    void paintListBoxItem(int rowNumber, juce::Graphics &g, int width, int height, bool rowIsSelected) override
-    {
-        if (rowIsSelected)
-        {
-            g.fillAll(GUI_ACCENT);
-        }
-        g.setColour(GUI_TEXT_WHITE);
-        g.drawText(listItems[rowNumber], 0, 0, width, height, juce::Justification::centredLeft);
-    }
-
-    void listBoxItemClicked(int row, const juce::MouseEvent &) override
-    {
-        DBG("Item clicked: " << listItems[row]);
-    }
-
-    void setItems(juce::StringArray stringArray)
-    {
-        listItems = stringArray;
-    }
-
-    juce::StringArray listItems;
+    SearchResultsListBoxModel(const juce::StringArray &items) : listItems(items) {}
+    int getNumRows() override;
+    void paintListBoxItem(int rowNumber, juce::Graphics &g, int width, int height, bool rowIsSelected) override;
+    void listBoxItemClicked(int row, const juce::MouseEvent &) override;
+    void setItems(juce::StringArray stringArray);
 private:
-    
+    juce::StringArray listItems;
 };
 
 #endif SEARCH_RESULT_LIST_H
