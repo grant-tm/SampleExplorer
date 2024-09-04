63926ea00fec44a5820f3304097d70957e902506
grant-tm <grant.t.morgan@gmail.com>
2024-09-03 20:03:21 -0700

    create key filter module

---
+++ Source/GUI Components/KeyFilter/KeyFilter.cpp
@@ -0,0 +1,40 @@
+#include "KeyFilter.h"
+
+KeyFilter::KeyFilter()
+{
+	juce::FontOptions labelFontOptions(18.f, juce::Font::plain);
+	label.setFont(labelFontOptions);
+	label.setJustificationType(juce::Justification::centredLeft);
+	label.setColour(juce::Label::textColourId, juce::Colour(0xFFD9D9D9));
+	addAndMakeVisible(label);
+	addAndMakeVisible(keyboard);
+	addAndMakeVisible(keyModeSelector);
+}
+
+KeyFilter::~KeyFilter(){}
+
+void KeyFilter::resized()
+{
+	auto bounds = getLocalBounds();
+
+	auto labelBounds = bounds.removeFromTop(30);
+	labelBounds.removeFromLeft(4);
+	label.setBounds(labelBounds);
+	
+	auto modeSelectorBounds = bounds.removeFromBottom(30);
+	modeSelectorBounds.removeFromLeft(12);
+	modeSelectorBounds.removeFromRight(12);
+	modeSelectorBounds.removeFromBottom(7);
+	keyModeSelector.setBounds(modeSelectorBounds);
+
+	auto keyboardBounds = bounds;
+	keyboardBounds.removeFromLeft(14);
+	keyboardBounds.removeFromRight(12);
+	keyboardBounds.removeFromBottom(7);
+	keyboard.setBounds(keyboardBounds);
+}
+
+void KeyFilter::paint(juce::Graphics &g)
+{
+	g.fillAll(GUI_PRIMARY_ELEV1);
+}
---
+++ Source/GUI Components/KeyFilter/KeyFilter.h
@@ -0,0 +1,24 @@
+#ifndef KEY_FILTER_H
+#define KEY_FILTER_H
+
+#include <JuceHeader.h>
+#include "../ColorPalette.h"
+#include "Keyboard.h"
+#include "ModeSelector.h"
+
+class KeyFilter : public juce::Component
+{
+public:
+	KeyFilter();
+	~KeyFilter();
+
+	void resized() override;
+	void paint(juce::Graphics &g) override;
+
+private:
+	juce::Label label = juce::Label("Key Filter Label", "Key");
+	Keyboard keyboard;
+	ModeSelector keyModeSelector = ModeSelector("Major", "Minor");
+};
+
+#endif // KEY_FILTER_H
